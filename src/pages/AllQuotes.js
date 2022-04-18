7fcafb40f53eef0f1a755057a425a251bd71e1c5
Jonathan Kromer <jdkromer@gmail.com>
2022-04-17 21:16:00 -0400

    Outputting Data on the Details Page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -2,7 +2,7 @@ import QuoteList from "../components/quotes/QuoteList";
 
 const Dummy_Quotes = [
     { id: 'q1', author: 'Max', text: 'Learning React is fun!'},
-    { id: 'q1', author: 'Maximilan', text: 'Learning React is great!'}
+    { id: 'q2', author: 'Maximilan', text: 'Learning React is great!'}
 ];
 
 const AllQuotes = () => {
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,13 +1,24 @@
 import { useParams, Route } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const Dummy_Quotes = [
+    { id: 'q1', author: 'Max', text: 'Learning React is fun!'},
+    { id: 'q2', author: 'Maximilan', text: 'Learning React is great!'}
+];
 
 const QuoteDetails = () => {
     const params = useParams();
+    const quote = Dummy_Quotes.find(quote => quote.id === params.quoteID);
+    
+    if(!quote){
+        return <p>No Quote Found!</p>;
+    }
+
     return (
         <section>
-            <h1>QuoteDetails Page</h1>
-            <p>{ params.quoteID }</p>
+            <HighlightedQuote text={quote.text} author={quote.author} />
             <Route path={`/quotes/${params.quoteID}/comments`}>
                 <Comments />
             </Route>
